260cd8ab0e82fbd63b96d702984135d244d57aa0
MigueliDeveloper <miguelidevelop@gmail.com>
2023-08-31 19:52:43 +0100

    holi

--- src/app/servicios/listaservicios.service.ts
+++ src/app/servicios/listaservicios.service.ts
@@ -8,10 +8,10 @@ export class ListaserviciosService {
   private listaServicios: Array<ServiciosBD>
   constructor() { 
     this.listaServicios = [
-                new ServiciosBD("100", "Consultoría","Servicio de consultoría sobre formación en programación frontend, backend y fullstack", 60,"./assets/media/consultoria.png", true), 
-                new ServiciosBD("101", "Formación Frontend","Servicio de formación sobre  programación frontend", 160,"./assets/media/frontend.jpg", true), 
-                new ServiciosBD("102", "Formación Backend","Servicio de formación sobre  programación Backend", 160,"./assets/media/backend.png", true),
-                new ServiciosBD("103", "Formación FullStack","Servicio de formación sobre  programación FullStock", 300,"./assets/media/fullstack.jpg", true)
+                new ServiciosBD("100", "Atención al cliente","Este servicio se encarga de responder a las consultas y solicitudes de los clientes. Puede estar relacionado con otros servicios, como el servicio de ventas o el servicio de soporte técnico.", 60,"./assets/media/consultoria.png", true), 
+                new ServiciosBD("101", "Servicio de ventas","Este servicio se encarga de vender los productos o servicios de la empresa.", 160,"./assets/media/frontend.jpg", true), 
+                new ServiciosBD("102", "Servicio de soporte técnico","Este servicio se encarga de resolver los problemas técnicos que los clientes puedan tener con los productos o servicios de la empresa", 160,"./assets/media/backend.png", true),
+                new ServiciosBD("103", "Servicio de marketing","Este servicio se encarga de promover los productos o servicios de la empresa. Puede estar relacionado con otros servicios, como el servicio de ventas o el servicio de atención al cliente1", 300,"./assets/media/fullstack.jpg", true)
               ]
   }
   getServicios():Array<ServiciosBD>{
