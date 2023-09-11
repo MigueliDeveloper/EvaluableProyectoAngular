b2bfafcf479c6b68753b6726bdc0f75745c1fb53
MigueliDeveloper <miguelidevelop@gmail.com>
2023-09-11 19:32:51 +0100

    holi

--- servenode/app/controller/usuariosController.js
+++ servenode/app/controller/usuariosController.js
@@ -29,16 +29,58 @@ exports.actualizarUsuarios = async (req, res) => {
 		const { nombre, descripcion, categoria, duracion, temas} = req.body;
 		let usuarios = await Usuarios.findById(req.params.id);
 		if(!curso) {
-			res.status(404).json({ msg: 'No existe el Curso' })
+			res.status(404).json({ msg: 'No existe el Usuario' })
 		} 
+		usuarios.name = nameUsuarios;
+		usuarios.description = description;
+		usuarios.category = category;
+		usuarios.duracion = duracion;
+		usuarios.temas = temas;
+		usuarios.image = image;
+
+		usuarios = await Usuarios.findOneAndUpdate({ _id: req.params.id }, curso, { new: true} )
+		res.json(usuarios);
+	} catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
 
 
+exports.obtenerUsuarios = async (req, res) => {
+	try {
+        let usuario = await Usuarios.findById(req.params.id);
+        if(!usuario) {
+            res.status(404).json({ msg: 'No existe los Usuarios' })
+        }     
+        res.json(usuario);       
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
 
+exports.eliminarUsarios = async (req, res) => {
+	try {
 
-	}
-
-
+		let usuarios = await Usuarios.findById(req.params.id);
+		if(!usuarios){
+			res.status(404).json({ msg: 'No existe los Usuarios'})
+		}
+		await Curso.findOneAndRemove({ _id: req.params.id })
+        res.json({ msg: 'Curso eliminado con exito' });       
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 
 }
+
+
+
+
+
+
+
 
 
