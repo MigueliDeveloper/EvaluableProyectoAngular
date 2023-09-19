63a2ca3cdcc9c8e7819976ca91f6b754c6d99538
MigueliDeveloper <miguelidevelop@gmail.com>
2023-09-19 18:56:44 +0100

    holi

--- servenode/app/server.js
+++ servenode/app/server.js
@@ -6,29 +6,17 @@ const cors = require("cors")
 // Creamos el servidor
 const app = express()
 
-// const port = 3700
-// app.get('/', (req, res) =>{
-//     res.send('Servidor en linea por pantalla...')
-// })
-// const cursoRouters = require( './app/routes/server')
-// app.use(curosRouters)
 
-// Conexión con Base datos
-// mongoose.Promise = global.Promise
-// mongoose.connect('mongodb://127.0.0.1:27017/Appweb', {
-//     useNewUrlParser: true,
-// })
-// .then(() => {
-//     console.log("Conexión establecida...")
-// })
-// .catch(err => console.log(err))
-
-// Conectamos DB Cursos
 conectarDB();
 
 app.use(cors())
 
 app.use(express.json());
+
+app.listen(3001, () => {
+    console.log('El servidor está en servicio correctamente');
+
+})
 
 app.use('/api/usuarios', require('./app/usuariosroutes'));
 app.use('/api/tareas', require('./app/tareasroutes'));
